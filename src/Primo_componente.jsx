b96d3b679c8f8fbdca3445037c130e5c2ec3dba2
valecant <valerio.cantore@esmo.org>
2025-09-21 00:13:09 +0200

    add hooks

--- src/Primo_componente.jsx
+++ src/Primo_componente.jsx
@@ -1,12 +1,31 @@
+// ...existing code...
+import React, { useState, useEffect } from "react";
+
 function Primo_componente() {
-  
-  return(
+  const [string, setString] = useState("io sono un componente il mo numero preferito è : ");
+  const [counter, setCounter] = useState(15);
 
-   <>
-        <h2>io sono un componente!</h2>
-   </>
-  )
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCounter(counter => counter + 1);
 
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
+
+  useEffect(() => {
+    setString(string => "io sono un componente il mo numero preferito è : " + counter);
+  }, [counter]);
+
+
+
+  return (
+    <>
+      <h2>{string}</h2>
+      <p>Counter: {counter}</p>
+    </>
+  );
 }
 
 export default Primo_componente
